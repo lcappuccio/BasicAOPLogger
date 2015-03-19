86e97b30e522895afba3c305aa6911a0b64c39a8
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-03-19 17:09:58 +0100

    spring dependencies

---
+++ BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/pojo/Logger.java
@@ -0,0 +1,16 @@
+/**
+ * $Id$
+ *
+ * @author lcappuccio
+ * @date 19/03/2015 17:08
+ *
+ * Copyright (C) 2015 Scytl Secure Electronic Voting SA
+ *
+ * All rights reserved.
+ *
+ */
+package org.systemexception.basicaoplogger.pojo;
+
+public class Logger {
+
+}
