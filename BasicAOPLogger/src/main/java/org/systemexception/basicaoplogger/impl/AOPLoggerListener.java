e5b0e1fc1d9dc760038e8a37b5f6d8ec513546c3
Leonardo Cappuccio <leo@systemexception.org>
2015-03-19 23:48:45 +0100

    aspect only around

--- BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/impl/AOPLoggerListener.java
+++ BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/impl/AOPLoggerListener.java
@@ -8,8 +8,8 @@ package org.systemexception.basicaoplogger.impl;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.systemexception.basicaoplogger.api.AOPLogger;
 
 @Aspect
@@ -17,16 +17,9 @@ public class AOPLoggerListener {
 
 	private AOPLogger aopLogger;
 
-	@Before("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage())")
+	@Around("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage())")
 	public void logBefore(JoinPoint joinPoint) {
 		aopLogger = AOPLoggerImpl.getFor(joinPoint.getTarget().getClass());
-		aopLogger.info("Before: " + joinPoint.getClass().getCanonicalName());
+		aopLogger.info(joinPoint.getClass().getCanonicalName());
 	}
-
-	@After("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage())")
-	public void logAfter(JoinPoint joinPoint) {
-		aopLogger = AOPLoggerImpl.getFor(joinPoint.getClass());
-		aopLogger.info("After: " + joinPoint.getClass().getCanonicalName());
-	}
-
 }
