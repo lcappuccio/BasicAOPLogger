8c6d11737a2304c3ed5b91aa531d9daf056f018c
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-03-19 18:28:50 +0100

    refactor

--- BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/pojo/Logger.java
+++ BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/pojo/Logger.java
@@ -2,7 +2,7 @@
  * $Id$
  *
  * @author lcappuccio
- * @date 19/03/2015 17:08
+ * @date 19/03/2015 18:27
  *
  * Copyright (C) 2015 Scytl Secure Electronic Voting SA
  *
@@ -11,27 +11,6 @@
  */
 package org.systemexception.basicaoplogger.pojo;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-
-@Aspect
 public class Logger {
-	
-	@Before("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage())")
-	public void logBefore(JoinPoint joinPoint) {
- 
-		System.out.println("logBefore() is running!");
-		System.out.println("hijacked : " + joinPoint.getSignature().getName());
-		System.out.println("******");
-	}
-	
-	@After("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage())")
-	public void logAfter(JoinPoint joinPoint) {
- 
-		System.out.println("logAfter() is running!");
-		System.out.println("hijacked : " + joinPoint.getSignature().getName());
-		System.out.println("******");
-	}
+
 }
