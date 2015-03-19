0ade1d74abd3a37537aebfde10b94d20490bcacc
Leonardo Cappuccio <leo@systemexception.org>
2015-03-19 22:19:24 +0100

    delete console logger

---
+++ BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/impl/AOPLoggerListener.java
@@ -0,0 +1,34 @@
+/**
+ *
+ * @author leo
+ * @date 19/03/2015 21:58
+ *
+ */
+package org.systemexception.basicaoplogger.impl;
+
+import java.util.logging.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.systemexception.basicaoplogger.api.AOPLogger;
+
+@Aspect
+public class AOPLoggerListener {
+
+	private AOPLogger aopLogger;
+
+	@Before("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage())")
+	public void logBefore(JoinPoint joinPoint) {
+		aopLogger = AOPLoggerImpl.getFor(joinPoint.getTarget().getClass());
+		aopLogger.info("Before: " + joinPoint.getClass().getCanonicalName());
+	}
+
+	@After("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage())")
+	public void logAfter(JoinPoint joinPoint) {
+
+		aopLogger = AOPLoggerImpl.getFor(joinPoint.getClass());
+		aopLogger.info("After: " + joinPoint.getClass().getCanonicalName());
+	}
+
+}
