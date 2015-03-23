8e88485057258ea5f45ae989ee9994d5ca6b2a72
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-03-23 18:12:24 +0100

    defect: pointcut not working

--- BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/impl/AOPLoggerListener.java
+++ BasicAOPLogger/src/main/java/org/systemexception/basicaoplogger/impl/AOPLoggerListener.java
@@ -9,18 +9,14 @@ package org.systemexception.basicaoplogger.impl;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.systemexception.basicaoplogger.api.AOPLogger;
 
 @Aspect
 public class AOPLoggerListener {
 
 	private AOPLogger aopLogger;
-	
-	@Pointcut("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage(..))")
-	private void logInfo(){}
 
-	@Around("logInfo()")
+	@Around("execution(* org.systemexception.basicaoplogger.pojo.Message.getMessage(..))")
 	public void logAround(JoinPoint joinPoint) {
 		aopLogger = AOPLoggerImpl.getFor(joinPoint.getTarget().getClass());
 		aopLogger.info(joinPoint.getTarget().getClass().getCanonicalName());
